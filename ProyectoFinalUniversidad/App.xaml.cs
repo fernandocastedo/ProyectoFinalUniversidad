a9c12b3f8df7e7362d60e3cf0fc71ee5393c6376
fernandocastedo <cmpfernando.ouac@gmail.com>
2025-06-04 10:48:13 -0400

    Add project files.

---
+++ ProyectoFinalUniversidad/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProyectoFinalUniversidad
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
